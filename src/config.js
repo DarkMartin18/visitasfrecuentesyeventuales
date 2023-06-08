acb7283b2147cfee9bfc71fb257649e281eb83bb
DarkMartin18 <darkmartin18@hotmail.com>
2023-06-07 23:11:21 -0700

    Chatbot y BD en la nube
    Chatbot y BD en la nube

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/test";
+  process.env.MONGODB_URI || "mongodb+srv://maerbarrondo:inRrqe83TQDvqaWT@cluster0.l8ei5dt.mongodb.net/test";
---
+++ src/public/js/codigo.js
@@ -0,0 +1,68 @@
+var sendBtn = document.getElementById("sendBtn");
+var textbox = document.getElementById("textbox");
+var chatcontainer = document.getElementById("chatContainer");
+
+var user = { message: "" };
+
+function SendMessage(userMessage) {
+  var messageElement = document.createElement("div");
+  messageElement.style.textAlign = "right";
+  messageElement.style.margin = "10px";
+  messageElement.innerHTML =
+    "<span> Yo: </span>" + "<span>" + userMessage + "</span>";
+  chatcontainer.appendChild(messageElement);
+}
+
+function chatResponse(userMessage) {
+  var chatbotmessage = "";
+  fetch('http://127.0.0.1:3000/chatbot', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ pregunta: userMessage })
+  })
+    .then(response => response.json())
+    .then(data => {
+      chatbotmessage = data.respuesta; 
+      var messageElement = document.createElement("div");
+      messageElement.style.textAlign = "left";
+      messageElement.style.margin = "10px";
+      messageElement.innerHTML =
+        "<span> PetBot: </span>" + "<span>" + chatbotmessage + "</span>";
+      chatcontainer.scrollTop = chatcontainer.scrollHeight;
+      
+      messageElement.animate(
+        [
+          { easing: "ease-in", opacity: 0.4 },
+          { opacity: 1 },
+          { direction: 1000 },
+        ],
+        {
+          duration: 1000,
+          fill: "forwards",
+        }
+      );
+
+      setTimeout(() => {
+        chatcontainer.appendChild(messageElement);
+      }, 1000);
+    })
+    .catch(error => {
+      console.error("Error en la solicitud al chatbot:", error);
+    });
+}
+
+sendBtn.addEventListener("click", function (e) {
+  var userMessage = textbox.value;
+
+  if (userMessage == "") {
+    textbox.placeholder = "Ingrese texto...";
+  } else {
+    let userMessageText = userMessage.trim();
+    user.message = userMessageText;
+    textbox.value = "";
+    SendMessage(userMessageText);
+    chatResponse(userMessageText);
+  }
+});
